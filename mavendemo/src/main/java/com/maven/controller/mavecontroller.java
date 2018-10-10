f7bf1fb9ca751421aa36273ec4f5309a2b950358
sebislove <1803116046@qq.com>
2018-10-10 13:53:32 +0800

    @author:amber 2018-10-10

---
+++ mavendemo/src/main/java/com/maven/controller/mavecontroller.java
@@ -0,0 +1,47 @@
+package com.maven.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mvn.entity.HelloWorld;
+
+/**
+ * Servlet implementation class mavecontroller
+ */
+@WebServlet("/action/hello.do")
+public class mavecontroller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private HelloWorld hello=new HelloWorld();   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public mavecontroller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out=response.getWriter();
+        out.print("<H1>"+hello.print()+"<H1>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ mavendemo/src/main/java/com/mvn/entity/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mvn.entity;
+
+public class HelloWorld {
+     public void say() {
+    	 System.out.println("hello maven!!!");
+     }
+     
+     public String print() {
+    	 return "hello world!";
+     }
+}
